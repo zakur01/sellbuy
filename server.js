1d07b303cfd3778ac1e8ce23b74d318dfa792215
Ulanchik Moldousupovchik <sunnyhako324@gmail.com>
2020-05-09 00:07:00 +0600

    Роутер для регистрация пользователя, настройка JWT токена

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const connectDB = require("./config/db");
 connectDB();
 
 app.use(express.json());
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
-
