952d1f676e65d3d0b6f1c1edbcc67adae877dcf1
Ulanchik Moldousupovchik <sunnyhako324@gmail.com>
2020-05-09 16:40:52 +0600

    удаление пользователя и профиля

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -36,35 +36,47 @@ router.get("/", async (req, res) => {
 });
 
 //обновление контактных данных или создание профиля при его отсутствии
-router.post("/", [
-  auth,
-  [check("contacts", "требуются контактные данные").not().isEmpty()],
-],
-async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  const { contacts } = req.body;
-  const profileFields = {};
-  profileFields.user = req.user.id;
-  if (contacts) profileFields.contacts = contacts;
+router.post(
+  "/",
+  [auth, [check("contacts", "требуются контактные данные").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { contacts } = req.body;
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (contacts) profileFields.contacts = contacts;
 
     try {
-    let profile = await Profile.findOne({ user: req.user.id })  
+      let profile = await Profile.findOne({ user: req.user.id });
 
-    if (profile) {
-      profile = await Profile.findOneAndUpdate(
-        { user: req.user.id },
-        { $set: profileFields },
-        { new: true }
-      );
-      return res.json(profile);
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("ошибка сервера");
     }
+  }
+);
 
-    profile = new Profile(profileFields);
-    await profile.save();
-    res.json(profile);
+//удаление пользователя
+router.delete("/", auth, async (req, res) => {
+  try {
+    await Profile.findOneAndDelete({ user: req.user.id });
+      await User.findOneAndDelete({ _id: req.user.id });
+    res.json({ msg: "пользователь удалён"})
   } catch (err) {
     console.error(err.message);
     res.status(500).send("ошибка сервера");
