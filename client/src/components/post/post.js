9cc33c4cc6bf93afd6f55afc006b76c6749faa89
Ulanchik Moldousupovchik <sunnyhako324@gmail.com>
2020-07-01 15:26:50 +0600

    fin

--- client/src/components/post/post.js
+++ client/src/components/post/post.js
@@ -20,7 +20,7 @@ const Post = ({ getPost, post: { post, loading },  match }) => {
     </Link>
         <PostItem post={post} showActions={false} />
         <div className="comments">
-        {post.comments.map((comment) => (
+        {post.comments.reverse().map((comment) => (
             <CommentItem key={comment._id} comment={comment} postId={post._id} />
             ))}
         </div>
